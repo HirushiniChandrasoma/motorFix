babed92ae5f3fa11b5f34530ca12b7929fcc1ce3
HirushiniChandrasoma <HirushiniRashmika.C@gmail.com>
2025-02-27 13:55:40 +0530

    did changes to html files

--- MotorFix/app1/views.py
+++ MotorFix/app1/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def ProfilePage(request):
+    return render(request,'profile.html')
+
+def SignupPage(request):
+    return render(request,'signup.html')
+
+def LoginPage(request):
+    return render(request,'login.html')
